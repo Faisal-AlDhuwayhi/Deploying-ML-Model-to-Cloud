db08fad38c63830b755c0f812ef0b7004b126846
Faisal Aldhuwayhi <alduohy@hotmail.com>
2025-01-21 22:42:30 +0300

    add project setup python file

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="Deploying ML model to Cloud with FastAPI",
+    version="1.0.0",
+    description="End-to-end ML model and pipeline project\
+          using DVC and FastAPI, applying CI/CD,\
+              deploying on Heroku cloud platform.",
+    author="Faisal",
+)
